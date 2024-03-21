5eb05289bf485e7ff61f90b81f889393c3a94030
Omar Yesid Zuniga Calderon <ozunigacalderon@gmail.com>
2024-03-20 22:40:24 -0500

    Changes: Creation of LoginRequest and RegisterRequest for authentication of authentication login and encoding password in registering.

---
+++ domain/auth/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.rallyce.Petroleum_Inventario.domain.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+    String username;
+    String password;
+}
---
+++ domain/auth/RegisterRequest.java
@@ -0,0 +1,24 @@
+package com.rallyce.Petroleum_Inventario.domain.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    String nombre;
+
+    String pais;
+
+    String ciudad;
+
+    String username;
+
+    String password;
+
+}
